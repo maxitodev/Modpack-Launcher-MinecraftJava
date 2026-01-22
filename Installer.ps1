e18ac950525b0c0eca9135634119ff3246674a01
Maxuriel <max.sanchezd13@gmail.com>
2026-01-22 16:46:00 -0600

    Increase Java heap size to 8G for improved performance in NeoForge launcher profile

--- Installer.ps1
+++ Installer.ps1
@@ -363,7 +363,7 @@ function Configure-LauncherProfile {
                 lastUsed = (Get-Date -Format "o")
                 icon = "Grass"
                 lastVersionId = "neoforge-21.1.37"
-                javaArgs = "-Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M"
+                javaArgs = "-Xmx8G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M"
             }
             
             # Agregar el nuevo perfil
